10c0a3d66f59df49396e008d052be9de8cd98061
mannen0 <17mannen@nao.net.it-chiba.ac.jp>
2017-06-03 15:42:13 +0900

    update ins.sh

--- ins.sh
+++ ins.sh
@@ -2,15 +2,15 @@
 
 sudo apt-get update
 sudo apt-get -y install git curl g++ make zlib1g-dev libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt-dev sqlite3 libsqlite3-dev nodejs
-git clone git://github.com/sstephenson/rbenv.git .rbenv
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 mkdir -p ~/.rbenv/plugins
 cd ~/.rbenv/plugins/
 git clone git://github.com/sstephenson/ruby-build.git
-rbenv install 2.4.1
-echo 'PATH=$HOME/.rbenv/versions/2.4.1/bin/:$PATH' >> ~/.bashrc
 cp ~/.bashrc ~/.bashrc.default
 source ~/.bashrc
+rbenv install 2.4.1
+echo 'PATH=$HOME/.rbenv/versions/2.4.1/bin/:$PATH' >> ~/.bashrc
 gem install rails
 bundle install
