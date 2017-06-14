55d3315355cb798197c948c1d86b9048990ab75f
mannen0 <17mannen@nao.net.it-chiba.ac.jp>
2017-06-14 15:40:42 +0900

    modified setup_rails.sh

--- setup_rails.sh
+++ setup_rails.sh
@@ -6,10 +6,10 @@ git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
 cp ~/.bashrc ~/.bashrc.default
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-echo 'PATH=$HOME/.rbenv/versions/2.4.1/bin:$PATH' >> ~/.bashrc
+#echo 'PATH=$HOME/.rbenv/versions/2.4.1/bin:$PATH' >> ~/.bashrc
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
-PATH=$HOME/.rbenv/versions/2.4.1/bin:$PATH
+#PATH=$HOME/.rbenv/versions/2.4.1/bin:$PATH
 mkdir -p ~/.rbenv/plugins
 cd ~/.rbenv/plugins/
 git clone git://github.com/sstephenson/ruby-build.git
