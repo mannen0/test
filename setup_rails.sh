ad5e11521989ab7deb7865bf807929434bc052a5
mannen0 <17mannen@nao.net.it-chiba.ac.jp>
2017-08-05 11:56:23 +0900

    modified setup_rails.sh

--- setup_rails.sh
+++ setup_rails.sh
@@ -4,10 +4,10 @@ sudo apt-get update
 sudo apt-get -y install git curl g++ make zlib1g-dev libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt-dev sqlite3 libsqlite3-dev nodejs
 git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
 cp ~/.bashrc ~/.bashrc.default
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="$HOME/.rbenv/shims:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 #echo 'PATH=$HOME/.rbenv/versions/2.4.1/bin:$PATH' >> ~/.bashrc
-export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH="$HOME/.rbenv/shims:$PATH"
 eval "$(rbenv init -)"
 #PATH=$HOME/.rbenv/versions/2.4.1/bin:$PATH
 mkdir -p ~/.rbenv/plugins
